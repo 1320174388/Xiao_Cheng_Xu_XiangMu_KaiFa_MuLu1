bc1117b0a514a265e9e0765a4a47cb8fb2350756
时广玉 <shiguangyu@dlaotianhuang.com>
2018-06-21 16:10:03 +0800

    2018/06/21 15:18 权限模块-v3-优化接口

--- route/right_route_v3_api.php
+++ route/right_route_v3_api.php
@@ -80,7 +80,7 @@ Route::group('v3/right_module/', function(){
      * 传值方式：POST 路由功能：执行添加权限操作
      */
     Route::post(
-        'right_route',
+        'right_route/:token',
         'right_module/v3.controller.RightController/rightAddRoute'
     );
     /**

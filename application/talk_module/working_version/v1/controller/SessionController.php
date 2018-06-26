3750e9e0d9c28646b11e47801e07d9e5e472a4fe
时广玉 <shiguangyu@dlaotianhuang.com>
2018-06-26 09:59:16 +0800

    2018/06/26 09:58 客户模块-v1-开发中

---
+++ application/talk_module/working_version/v1/controller/SessionController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  SessionController.php
+ *  创 建 者 :  Shi Guang Yu
+ *  创建日期 :  2018/06/26 09:50
+ *  文件描述 :  客户会话控制器
+ *  历史记录 :  -----------------------
+ */
+namespace app\talk_module\working_version\v1\controller;
+use think\Controller;
+
+class SessionController extends Controller
+{
+
+}

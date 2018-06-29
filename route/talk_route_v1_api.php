3c71ceaf390951f7e5e5e3476b4156101ad48172
时广玉 <shiguangyu@dlaotianhuang.com>
2018-06-29 17:40:17 +0800

    2015/06/29 17:31 更新项目-开发客服功能初始化接口

--- route/talk_route_v1_api.php
+++ route/talk_route_v1_api.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *  版权声明 :  地老天荒科技有限公司
- *  文件名称 :  take_route_v1_api.php
+ *  文件名称 :  talk_route_v1_api.php
  *  创 建 者 :  Shi Guang Yu
  *  创建日期 :  2018/06/29 17:20
  *  文件描述 :  客服功能模块接口路由
@@ -12,6 +12,6 @@
 // : 传值方式：消息推送接口地址
 // +------------------------------------------------------
 Route::get(
-    'v1/take_module/session_route',
-    'take_module/v1.controller.SessionController/sessionValue'
+    'v1/talk_module/session_route',
+    'talk_module/v1.controller.SessionController/sessionValue'
 );

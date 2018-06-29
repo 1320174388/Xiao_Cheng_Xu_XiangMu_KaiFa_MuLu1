387f46b893f5dceef8c60a0277d3ba13716289d7
时广玉 <shiguangyu@dlaotianhuang.com>
2018-06-29 21:11:38 +0800

    2015/06/29 17:31 更新项目-开发客服功能初始化接口

--- application/talk_module/working_version/v1/library/SessionLibrary.php
+++ application/talk_module/working_version/v1/library/SessionLibrary.php
@@ -23,9 +23,9 @@ class SessionLibrary
     public function checkSignature()
     {
         // 获取开发者验证请求的数据
-        $signature = request()->get('signature');
-        $timestamp = request()->get('timestamp');
-        $nonce     = request()->get('nonce');
+        $signature = (new Request)->get('signature');
+        $timestamp = (new Request)->get('timestamp');
+        $nonce     = (new Request)->get('nonce');
         // 获取配置文件的Token值
         $token = config('v1_config.Token');
         // 处理成数组

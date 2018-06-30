5827102f74a11ce75e4e664f4e85581183597bcf
shirui <18613370205@163.com>
2018-06-30 22:34:14 +0800

    客服模块V1版_添加控制器等 2018/06/30 22:34

---
+++ application/talk_module/working_version/v1/controller/LeavingsController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  LeavingsController.php
+ *  创 建 者 :  Shi Rui
+ *  创建日期 :  2018/06/30 22:11
+ *  文件描述 :  保存所有留言控制器
+ *  历史记录 :  -----------------------
+ */
+namespace app\talk_module\working_version\v1\controller;
+use think\Controller;
+use think\Request;
+use app\talk_module\working_version\v1\library\LeavingsLibrary;
+use app\talk_module\working_version\v1\service\LeavingsService;
+
+class LeavingsController extends Controller
+{}
---
+++ application/talk_module/working_version/v1/service/PeoplesServer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  PeoplesServer.php
+ *  创 建 者 :  Shi Rui
+ *  创建日期 :  2018/06/30 22:29
+ *  文件描述 :  留言人消息逻辑
+ *  历史记录 :  -----------------------
+ */
+namespace app\talk_module\working_version\v1\service;
+
+class PeoplesServer
+{
+
+}

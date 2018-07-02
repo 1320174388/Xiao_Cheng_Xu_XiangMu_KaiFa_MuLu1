2f25281f736cb2df428267f2449f6aa2ed332e6f
qyh <156457917@qq.com>
2018-07-02 11:17:40 +0800

    2018/07/02 11:17 ReplysDao

--- application/talk_module/working_version/v1/dao/ReplysDao.php
+++ application/talk_module/working_version/v1/dao/ReplysDao.php
@@ -12,5 +12,114 @@ use app\talk_module\working_version\v1\model\ReplyModel;
 
 class ReplyDao implements ReplysInterface
 {
-	
+	/**
+     * 名  称 : replySelect()
+     * 功  能 : 声明：获取单个或所有自动回复数据
+     * 输  入 : (string) $index => '自动回复触发主键';
+     * 输  出 : [ 'msg' => 'success', 'data' => '数据' ]
+     * 输  出 : [ 'msg' => 'error', 'data' => false ]
+     * 创  建 : 2018/07/02 10:36
+     */
+	public function replySelect($index='')
+	{
+		// 实例化自动回复信息model
+		$ReplyModel = new ReplyModel;
+		// 判断查询为单个回复数据还是所有回复数据
+		// 如果$index为空字符串,就查询所有
+		if($index = ''){
+			$list = $ReplyModel->select();
+		}else{
+			$list = $ReplyModel->where('index',$index)->find();
+		}
+		// 验证
+		if(!$list){
+			return returnData('error',false);
+		}
+		return returnData('success',$index);
+	}
+
+	/**
+     * 名  称 : replyCreate()
+     * 功  能 : 声明：添加自动回复数据
+     * 输  入 : (string) $sessionName => '自动回复名称';
+     * 输  入 : (string) $sessionType => '自动回复类型';
+     * 输  入 : (string) $sessionInfo => '自动回复内容';
+     * 输  出 : [ 'msg' => 'success', 'data' => true ]
+     * 输  出 : [ 'msg' => 'error',  'data' => false ]
+     * 创  建 : 2018/07/02 10:45
+     */
+	public function replyCreate($sessionName,$sessionType,$sessionInfo)
+	{
+		// 实例化自动回复信息model
+		$ReplyModel = new ReplyModel;
+		// 获取自动回复名称
+		$ReplyModel->session_name    = $sessionName;
+		// 获取自动回复类型
+		$ReplyModel->session_type    = $sessionType;
+		// 获取自动回复内容
+		$ReplyModel->session_content = $sessionInfo;
+		// 保存数据库
+		$data = $ReplyModel->save();
+		// 验证
+		if(!$data){
+			return returnData('error',false);
+		}
+		return returnData('success',true);
+	}
+
+	/**
+     * 名  称 : replyUpdate()
+     * 功  能 : 声明：修改自动回复数据
+     * 输  入 : (string) $index        => '回复信息主键';
+     * 输  入 : (string) $sessionName  => '自动回复名称';
+     * 输  入 : (string) $sessionType  => '自动回复类型';
+     * 输  入 : (string) $sessionInfo  => '自动回复内容';
+     * 输  出 : [ 'msg' => 'success', 'data' => true ]
+     * 输  出 : [ 'msg' => 'error',  'data' => false ]
+     * 创  建 : 2018/07/05 10:52
+     */
+	public function replyUpdate()
+	{
+		// 实例化自动回复信息model
+		$ReplyModel = new ReplyModel;
+
+		// 获取回复信息主键
+		$ReplyModel->session_index   = $index;
+		
+		// 进行修改
+		$res = $ReplyModel->save([
+			// 获取自动回复信息名称
+			$ReplyModel->session_name    = $sessionName;
+			// 获取自动回复信息类型
+			$ReplyModel->session_type  	 = $sessionType;
+			// 获取自动回复信息内容
+			$ReplyModel->session_content = $sessionInfo;
+		],['session_index'=>$index]);
+		// 验证
+		if(!$res){
+			return returnData('error',false);
+		}
+		return returnData('success',true);
+	}
+
+	/**
+     * 名  称 : replyDelete()
+     * 功  能 : 声明：删除自动回复数据
+     * 输  入 : (string) $index => '自动回复触发主键';
+     * 输  出 : [ 'msg' => 'success', 'data' => true ]
+     * 输  出 : [ 'msg' => 'error',  'data' => false ]
+     * 创  建 : 2018/07/05 11:02
+     */
+	public function replyDelete($index)
+	{
+		// 实例化自动回复信息model
+		$ReplyModel = new ReplyModel;
+		// 执行删除操作
+		$del = $ReplyModel->where('session_index',$index)->delete();
+		// 验证
+		if(!$del){
+			return returnData('error',false);
+		}
+		return returnData('success',true);
+	}
 }
--- application/talk_module/working_version/v1/dao/ReplysInterface.php
+++ application/talk_module/working_version/v1/dao/ReplysInterface.php
@@ -3,8 +3,8 @@
  *  版权声明 :  地老天荒科技有限公司
  *  文件名称 :  ReplysInterface.php
  *  创 建 者 :  Shi Guang Yu
- *  创建日期 :  2018/07/02 09:15
- *  文件描述 :  获取、添加、修改、自动回复信息
+ *  创建日期 :  2018/07/05 09:15
+ *  文件描述 :  获取、添加、修改、删除管理员信息
  *  历史记录 :  -----------------------
  */
 namespace app\right_module\working_version\v3\dao;
@@ -17,25 +17,22 @@ interface ReplysInterface{
      * 输  入 : (string) $index => '自动回复触发主键';
      * 创  建 : 2018/07/05 09:15
      */
-    public function replySelect($index='');
+    public function replySelect($index);
 
     /**
      * 名  称 : replyCreate()
      * 功  能 : 声明：添加自动回复数据
-     * 输  入 : (string) $sessionName => '自动回复名称';
-     * 输  入 : (string) $sessionType => '自动回复类型';
-     * 输  入 : (string) $sessionInfo => '自动回复内容';
+     * 输  入 :
+     * 输  出 :
      * 创  建 : 22018/07/05 09:15
      */
-    public function replyCreate($sessionName,$sessionType,$sessionInfo);
+    public function replyCreate();
 
     /**
      * 名  称 : replyUpdate()
      * 功  能 : 声明：修改自动回复数据
-     * 输  入 : (string) $index        => '回复信息主键';
-     * 输  入 : (string) $sessionName  => '自动回复名称';
-     * 输  入 : (string) $sessionType  => '自动回复类型';
-     * 输  入 : (string) $sessionInfo  => '自动回复内容';
+     * 输  入 :
+     * 输  出 :
      * 创  建 : 2018/07/05 09:15
      */
     public function replyUpdate();
@@ -44,6 +41,7 @@ interface ReplysInterface{
      * 名  称 : replyUpdate()
      * 功  能 : 声明：删除自动回复数据
      * 输  入 : (string) $index => '自动回复触发主键';
+     * 输  出 :
      * 创  建 : 2018/07/05 09:15
      */
     public function replyDelete($index);

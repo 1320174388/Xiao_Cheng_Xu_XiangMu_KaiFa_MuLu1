b88e93714ad2a1bd27280a0ddb271cbda6a64c4e
qyh <156457917@qq.com>
2018-07-02 09:50:18 +0800

    2018/07/02 9:49 加了一个ReplysDao层

---
+++ application/talk_module/working_version/v1/dao/ReplysDao.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  ReplysDao.php
+ *  创 建 者 :  Qi Yun Hai
+ *  创建日期 :  2018/07/02 09:43
+ *  文件描述 :  数据持久层,操作Reply：Model模型处理数据
+ *  历史记录 :  -----------------------
+ */
+namespace app\talk_module\working_version\v1\dao;
+use app\talk_module\working_version\v1\model\ReplyModel;
+
+class ReplyDao implements ReplysInterface
+{
+	
+}

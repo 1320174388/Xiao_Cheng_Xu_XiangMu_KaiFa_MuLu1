9e71d1075b8675793249d04d922aa9a241ec03ca
时广玉 <shiguangyu@dlaotianhuang.com>
2018-07-02 11:54:04 +0800

    2018/7/02 10:33 客服功能-开发中

---
+++ application/talk_module/working_version/v1/controller/ProblemController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  ProblemController.php
+ *  创 建 者 :  Shi Guang Yu
+ *  创建日期 :  2018/07/02 11:43
+ *  文件描述 :  用户提问消息处理控制器
+ *  历史记录 :  -----------------------
+ */
+namespace app\talk_module\working_version\v1\controller;
+use think\Controller;
+
+class ProblemController extends Controller
+{
+    /**
+     * 名  称 : problemValue()
+     * 功  能 : 处理用户提问信息函数
+     * 变  量 : --------------------------------------
+     * 输  入 : (string) -----------------------------
+     * 输  出 : {"errNum":0,"retMsg":"提问成功","retData":true}
+     * 创  建 : 2018/07/02 11:43
+     */
+    public function problemValue()
+    {
+        
+    }
+}

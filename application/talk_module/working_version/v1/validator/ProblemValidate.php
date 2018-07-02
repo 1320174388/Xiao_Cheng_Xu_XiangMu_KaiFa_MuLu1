7242f218dcc2cc642f543f127092a7de696db289
时广玉 <shiguangyu@dlaotianhuang.com>
2018-07-02 18:31:34 +0800

    2018/07/02 18:31 客服功能-v1-开发数据验证

---
+++ application/talk_module/working_version/v1/validator/ProblemValidate.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ *  版权声明 :  地老天荒科技有限公司
+ *  文件名称 :  ProblemValidate.php
+ *  创 建 者 :  Shi Rui
+ *  创建日期 :
+ *  文件描述 :  验证获取数据的逻辑
+ *  历史记录 :  -----------------------
+ */
+namespace app\talk_module\working_version\v1\validator;
+class ProblemValidate
+{
+
+}

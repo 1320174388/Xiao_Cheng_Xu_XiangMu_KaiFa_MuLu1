081dd08cf0d3f1af1d46607d8091016fad7a36d7
qyh <156457917@qq.com>
2018-07-04 16:44:30 +0800

    模板消息推送初步完成Dao层与service层 2018/07/04 16:44

--- application/push_module/working_version/v1/service/PushService.php
+++ application/push_module/working_version/v1/service/PushService.php
@@ -1,10 +1,10 @@
 <?php
 /**
  *  版权声明 :  地老天荒科技有限公司
- *  文件名称 :  PushService.php
- *  创 建 者 :  Shi Guang Yu
- *  创建日期 :  2018/07/04 14:24
- *  文件描述 :  推送消息实现叶业务逻辑层代码
+ *  文件名称 :  PushServer.php
+ *  创 建 者 :  Qi Yun Hai
+ *  创建日期 :  2018/07/04 16:40
+ *  文件描述 :  批量发送模板消息逻辑
  *  历史记录 :  -----------------------
  */
 namespace app\push_module\working_version\v1\service;
@@ -22,6 +22,11 @@ class PushService
      */
     public function appFormId($userToken,$formId)
     {
-
+        // 引入PushDao层
+        $push = (new PushDao())->pushCreate($userToken,$formId);
+        // 验证
+        if($push['msg'=='error']) return returnData('error');
+        // 返回数据格式
+        return returnData('success',$push['data']);
     }
 }
